663eac6a5ec4d16ea123f44a4bbc592587484af8
deepika <deepikasoni69068@gmail.com>
2026-01-09 23:31:36 +0530

    customer view changes

--- resources/views/admin/customers/partials/actions.blade.php
+++ resources/views/admin/customers/partials/actions.blade.php
@@ -5,7 +5,7 @@
     <button type="button" class="btn btn-info btn-sm viewCustomer" data-id="{{ $row->id }}">
         <i class="fa fa-eye"></i>
     </button>
-    <a href="{{ url('admin/customers/'.$row->id.'/docs' ) }}" class="btn btn-warning btn-sm">
+    <!-- <a href="{{ url('admin/customers/'.$row->id.'/docs' ) }}" class="btn btn-warning btn-sm">
         <i class="fa fa-file-arrow-up"></i>
-    </a>
+    </a> -->
 </div>
